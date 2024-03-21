da700ca4dfdb034f2fd592646395d1c4b880d615
McNal <lpmkonji977@gmail.com>
2024-03-21 16:40:17 +0900

    build 수정

--- app/api/mysql/heang/[id]/route.js
+++ app/api/mysql/heang/[id]/route.js
@@ -1,12 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import connection from '../../connect';
 
-/**
- *
- * @param {NextRequest} req
- * @param {*} param1
- */
 export async function GET(req, { params }) {
-	const [rows, fields] = await connection.execute(`SELECT * from sss WHERE ab_cd = ${params.id}`);
-	return NextResponse.json({ data: rows }, { status: 200 });
+	// const [rows, fields] = await connection.execute(`SELECT * from sss WHERE ab_cd = ${params.id}`);
+	return NextResponse.json({ data: {} }, { status: 200 });
 }

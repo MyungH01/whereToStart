63f8a23de08616827f37db04f38fcf914b05ae24
McNal <lpmkonji977@gmail.com>
2024-03-21 16:56:34 +0900

    build 수정

--- app/api/mysql/connect.js
+++ app/api/mysql/connect.js
@@ -1,9 +1,11 @@
 import mysql from 'mysql2/promise';
-const connection = await mysql.createConnection({
-	host: process.env.MYSQL_HOST,
-	user: process.env.MYSQL_USER,
-	password: process.env.MYSQL_PW,
-	database: process.env.MYSQL_DB,
-});
 
-export default connection;
+export default async function connection() {
+	const connection = await mysql.createConnection({
+		host: process.env.MYSQL_HOST,
+		user: process.env.MYSQL_USER,
+		password: process.env.MYSQL_PW,
+		database: process.env.MYSQL_DB,
+	});
+	return connection;
+}
--- app/api/mysql/heang/[id]/route.js
+++ app/api/mysql/heang/[id]/route.js
@@ -2,6 +2,6 @@ import { NextRequest, NextResponse } from 'next/server';
 import connection from '../../connect';
 
 export async function GET(req, { params }) {
-	// const [rows, fields] = await connection.execute(`SELECT * from sss WHERE ab_cd = ${params.id}`);
+	const [rows, fields] = await connection().execute(`SELECT * from sss WHERE ab_cd = ${params.id}`);
 	return NextResponse.json({ data: {} }, { status: 200 });
 }

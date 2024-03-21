97aed0af2209e3a2abe75e580a87340796b73bb2
McNal <lpmkonji977@gmail.com>
2024-03-21 16:52:09 +0900

    build 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		serverComponentsExternalPackages: ['mysql2'],
-	},
+	experimental: {},
 	images: {
 		remotePatterns: [
 			{
